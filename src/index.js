29f747b8a27644406910f90c6383b5381dca6f2c
hitot <91715186+hitot@users.noreply.github.com>
2021-10-01 17:09:53 +0900

    変数宣言確認

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import "./styles.css";
+/**
+ * comment
+ */
+var val1 = "var変数";
+console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let val2 = 0;
+console.log(val2);
